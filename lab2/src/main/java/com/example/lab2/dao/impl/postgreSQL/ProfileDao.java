2bc9a4def5c3e3bc04ac30683fe17e8705dc117a
akaeyuhi <andrvostr0101@gmail.com>
2025-05-20 17:57:46 +0300

    feat: separate ProfileDao for postgres

---
+++ lab2/src/main/java/com/example/lab2/dao/impl/postgreSQL/ProfileDao.java
@@ -0,0 +1,18 @@
+package com.example.lab2.dao.impl.postgreSQL;
+
+import com.example.lab2.dao.AbstractDao;
+import com.example.lab2.entities.Invitation;
+import com.example.lab2.entities.Profile;
+
+import java.util.Collection;
+
+public interface ProfileDao extends AbstractDao<Profile> {
+    Profile findByUsername(String username);
+    Profile findById(Long id);
+    Collection<Profile> findByText(String string);
+
+    void newProfile(Profile profile);
+    void addInvitation(Profile sender, Profile receiver, Invitation invitation);
+    void deleteInvitation(Profile sender, Profile receiver, Invitation invitation);
+    void acceptInvitation(Profile sender, Profile receiver, Invitation invitation);
+}
