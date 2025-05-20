679943f88d30082305f5aa1018210ea43b3625b5
akaeyuhi <andrvostr0101@gmail.com>
2025-05-20 17:52:32 +0300

    feat: AbstractDaoImpl for JPA

---
+++ lab2/src/main/java/com/example/lab2/dao/impl/postgreSQL/AbstractDaoImpl.java
@@ -0,0 +1,41 @@
+package com.example.lab2.dao.impl.postgreSQL;
+
+import com.example.lab2.dao.AbstractDao;
+import jakarta.persistence.*;
+import java.util.Collection;
+
+public abstract class AbstractDaoImpl<T> implements AbstractDao<T> {
+    @PersistenceContext(unitName = "LabPU")
+    protected EntityManager em;
+    private final Class<T> clazz;
+
+    protected AbstractDaoImpl(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public T get(Long id) {
+        return em.find(clazz, id);
+    }
+
+    @Override
+    public Collection<T> findAll() {
+        return em.createQuery("SELECT e FROM " + clazz.getSimpleName() + " e", clazz)
+                .getResultList();
+    }
+
+    @Override
+    public void insert(T entity, boolean generatedId) {
+        em.persist(entity);
+    }
+
+    @Override
+    public void update(T entity) {
+        em.merge(entity);
+    }
+
+    @Override
+    public void delete(T entity) {
+        em.remove(em.contains(entity) ? entity : em.merge(entity));
+    }
+}
