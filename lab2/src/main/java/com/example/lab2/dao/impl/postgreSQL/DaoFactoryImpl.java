56e5d448779fc52ab250949c89d8e5268afac7d2
akaeyuhi <andrvostr0101@gmail.com>
2025-05-20 17:57:31 +0300

    feat: DaoFactoryImpl for postgres

---
+++ lab2/src/main/java/com/example/lab2/dao/impl/postgreSQL/DaoFactoryImpl.java
@@ -0,0 +1,18 @@
+package com.example.lab2.dao.impl.postgreSQL;
+
+import com.example.lab2.dao.DaoFactory;
+import com.example.lab2.dao.ProfileDao;
+import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
+
+@Stateless
+public class DaoFactoryImpl implements DaoFactory {
+
+    @EJB
+    private ProfileDao profileDao;
+
+    @Override
+    public ProfileDao getProfileDao() {
+        return profileDao;
+    }
+}
